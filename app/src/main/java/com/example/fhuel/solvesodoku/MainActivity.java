8794bf37607b7be3766c625af68dc635a6d201ea
Mai65 <finnh.spam@outlook.de>
2018-08-06 21:38:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fhuel/solvesodoku/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.fhuel.solvesodoku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void ButtonField(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/fhuel/solvesodoku/awtrew.java
@@ -0,0 +1,18 @@
+package com.example.fhuel.solvesodoku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class awtrew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_awtrew);
+    }
+
+    public void onCasoifj(View view) {
+
+    }
+}
